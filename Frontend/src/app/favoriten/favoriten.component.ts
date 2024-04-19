5e82c196696eb7d66ed573e2dd529c79e3785b05
Kowalski187 <olivernix31@gmail.com>
2024-04-19 16:37:59 +0200

    frontend erstellt

---
+++ Frontend/src/app/favoriten/favoriten.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favoriten',
+  standalone: true,
+  imports: [],
+  templateUrl: './favoriten.component.html',
+  styleUrl: './favoriten.component.scss'
+})
+export class FavoritenComponent {
+
+}
