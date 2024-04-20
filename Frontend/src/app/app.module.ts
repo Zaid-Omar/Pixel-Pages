dce9a9626b95e231f0f3fa46ba2a0157cb41a305
Kowalski187 <olivernix31@gmail.com>
2024-04-21 01:01:56 +0200

    apis starten

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
+
