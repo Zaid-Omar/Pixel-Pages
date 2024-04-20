0aad5b21a86a2a0d0de3904ea1a317826a252b80
Kowalski187 <olivernix31@gmail.com>
2024-04-20 13:54:41 +0200

    Homepage erstellung von Sachen und Searchbar

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,12 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  searchTerm: string = '';
+  books: any[] = [
+    {
+      title: 'Buch 1',
+      isbn: '1234567890',
+      coverUrl: 'buchcover1.jpg'
+    },
+    {
+      title: 'Buch 2',
+      isbn: '0987654321',
+      coverUrl: 'buchcover2.jpg'
+    },
+    {
+      title: 'Buch 3',
+      isbn: '1357924680',
+      coverUrl: 'buchcover3.jpg'
+    },
+    {
+      title: 'Buch 1',
+      isbn: '1234567890',
+      coverUrl: 'buchcover1.jpg'
+    },
+    {
+      title: 'Buch 2',
+      isbn: '0987654321',
+      coverUrl: 'buchcover2.jpg'
+    },
+    {
+      title: 'Buch 3',
+      isbn: '1357924680',
+      coverUrl: 'buchcover3.jpg'
+    },
+    {
+      title: 'Buch 1',
+      isbn: '1234567890',
+      coverUrl: 'buchcover1.jpg'
+    },
+    {
+      title: 'Buch 2',
+      isbn: '0987654321',
+      coverUrl: 'buchcover2.jpg'
+    },
+    {
+      title: 'Buch 3',
+      isbn: '1357924680',
+      coverUrl: 'buchcover3.jpg'
+    },
+  ];
+  filteredBooks: any[] = [];
 
+  constructor() {
+    this.filteredBooks = this.books;
+  }
+
+  searchBooks() {
+    this.filteredBooks = this.books.filter(book =>
+      book.title.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      book.isbn.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
 }
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [HomeComponent]
+})
+export class HomeModule {}
