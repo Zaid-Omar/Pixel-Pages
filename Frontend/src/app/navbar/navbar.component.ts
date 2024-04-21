cedcf0667671add9e0783cb42d925d9491276195
Kowalski187 <olivernix31@gmail.com>
2024-04-21 15:49:19 +0200

    buchngen angepasst

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -10,6 +9,36 @@ import { RouterLink } from '@angular/router';
 })
 export class NavbarComponent {
   activeIndex: number = 0;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        switch (event.url) {
+          case '/':
+            this.activeIndex = 0;
+            break;
+          case '/buchungen':
+            this.activeIndex = 1;
+            break;
+          case '/favoriten':
+            this.activeIndex = 2;
+            break;
+          case '/kontakt':
+            this.activeIndex = 3;
+            break;
+          case '/login':
+            this.activeIndex = -1;
+            break;
+          case '/signup':
+            this.activeIndex = -1;
+            break;
+          default:
+            this.activeIndex = 0;
+            break;
+        }
+      }
+    });
+  }
 
   resetActiveIndex() {
     this.activeIndex = -1;
