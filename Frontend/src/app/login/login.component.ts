59d7a6e8d2c8477cebce294fa4de7decd9c8df4b
Kowalski187 <olivernix31@gmail.com>
2024-04-22 12:00:58 +0200

    validation

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,21 +1,28 @@
-import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {ReqRes} from "../entity/ReqRes";
 import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 import {LoginEntity} from "../entity/LoginEntity";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {User} from "../entity/User";
 import {Media} from "../entity/MediaEntity";
-
 import { DomSanitizer } from '@angular/platform-browser';
+import { FormGroup } from '@angular/forms';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+
+
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, HttpClientModule, FormsModule],
+  imports: [RouterLink, HttpClientModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  @Output() dataEvent = new EventEmitter<string>();
+  loginForm: FormGroup;
   vorname:any;
   nachname:any;
   benutzername: any;
@@ -24,23 +31,34 @@ export class LoginComponent {
   token: any;
   bild:any;
   ok=false;
-constructor(private http: HttpClient,private sanitizer: DomSanitizer) {}
-
+constructor(private http: HttpClient,
+            private sanitizer: DomSanitizer,
+            private formBuilder: FormBuilder,
+) {
+  this.loginForm = this.formBuilder.group({
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required],
+  })
+}
   public signIn(): void {
     const login = new LoginEntity(this.email, this.passwort);
     this.http.post<ReqRes>("http://localhost:8080/api/auth/signin", login)
       .subscribe(
         (response: ReqRes) => {
-          this.token=response.token;
-          localStorage.setItem("token", response.token);
-          console.log('Token:', response.token);
+          if (response.token) {
+            this.token = response.token;
+            localStorage.setItem("token", response.token);
+            console.log('Token:', response.token);
+            console.log('Anmeldung erfolgreich');
+          } else {
+            console.log('Anmeldung fehlgeschlagen');
+          }
         },
         error => {
           console.error('Fehler bei der Anmeldung:', error);
         }
       );
   }
-
 
 
   public getAllUers(){
@@ -58,13 +76,12 @@ constructor(private http: HttpClient,private sanitizer: DomSanitizer) {}
        }
     );
   }
-  public getAllMedia(){
 
+  public getAllMedia(){
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${localStorage.getItem("token")}`
     });
     const options = { headers: headers };
-
     this.http.get<Media[]>("http://localhost:8080/api/media/getAll",options)
        .subscribe(
        (response: Media[])=>{
@@ -72,7 +89,13 @@ constructor(private http: HttpClient,private sanitizer: DomSanitizer) {}
             console.log(response);
        }
     );
-    let ok=true;
+  }
+
+  test() {
+  console.log('test')
+    if (this.loginForm.valid) {
+      console.log('gut')
+    }
   }
 }
 // ../../assets/Coffee Book Cafe Library Tea Cup Shop Refresh Friend Restaurant Food Logo, Coffee, Book, Cup PNG and Vector with Transparent Background for Free Download.jpeg
