5e1655f27f0d2ac9d14acbca9012388a5ab57dbe
zomar <zzaid247@gmail.com>
2024-04-22 15:50:14 +0200

    add provideHttpClient()

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -3,9 +3,10 @@ import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(),
-    provideToastr()
+    provideToastr(),provideHttpClient()
   ]
 };
