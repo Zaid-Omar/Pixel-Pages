8a4b56c7d1bf3e49d3ddcf000bd190aee82baa6c
zomar <zzaid247@gmail.com>
2024-04-24 20:39:45 +0200

    added ISBN

--- Backend/src/main/java/com/example/backend/controller/ResverierungController.java
+++ Backend/src/main/java/com/example/backend/controller/ResverierungController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reservierung/")
+@RequestMapping("/api/reservierung")
 @CrossOrigin
 public class ResverierungController {
     @Autowired
