2bd55428447ddcf1b001ebc6781f730b380da683
Kowalski187 <olivernix31@gmail.com>
2024-04-26 16:45:53 +0200

    reservioerung

--- Frontend/src/app/services/reservierung.service.ts
+++ Frontend/src/app/services/reservierung.service.ts
@@ -37,26 +37,32 @@ export class ReservierungService {
 //* ---------------------------  Reservierung APIS  ---------------------------- *//
 
   public getAllReservierung(): Observable<ReqRes> {
+    this.initializeHeaders();
     return this.http.get<ReqRes>(`${this.baseUrl}/getAll`, this.options);
   }
 
   public getByIdReservierung(resv: ReqRes): Observable<ReqRes> {
+    this.initializeHeaders();
     return this.http.get<ReqRes>(`${this.baseUrl}/getById/${resv}`, this.options);
   }
 
   public getByUserReservierung(): Observable<ReqRes> {
+    this.initializeHeaders();
     return this.http.get<ReqRes>(`${this.baseUrl}/getByUser`, this.options);
   }
 
   public addReservierung(resv: Reservierung): Observable<Reservierung> {
+    this.initializeHeaders();
     return this.http.post<Reservierung>(`${this.baseUrl}/add`, resv, this.options);
   }
 
   public updateReservierung (resv: ReqRes): Observable<ReqRes> {
+    this.initializeHeaders();
     return this.http.put<ReqRes>(`${this.baseUrl}/update`, resv, this.options);
   }
 
   public deleteReservierung (resv: ReqRes): Observable<ReqRes> {
+    this.initializeHeaders();
     return this.http.delete<ReqRes>(`${this.baseUrl}/delete/${resv}`, this.options);
   }
 }
