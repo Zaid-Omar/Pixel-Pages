4d572eeaf5da1de01a776178ff456aac4edf2637
zomar <zzaid247@gmail.com>
2024-05-18 13:20:39 +0200

    vorschlag user Zaid

--- Backend/src/main/java/com/example/backend/service/VorschlagService.java
+++ Backend/src/main/java/com/example/backend/service/VorschlagService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 
+import com.example.backend.entity.User;
 import com.example.backend.entity.Vorschlag;
 import com.example.backend.repository.VorschlagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,16 @@ public class VorschlagService {
 
     @Autowired
     private VorschlagRepository vorschlagRepository;
+    @Autowired
+    private UserService userService;
 
 
     public ResponseEntity<Vorschlag> addVorschlag(Vorschlag vorschlag) {
+        if (!userService.findById(vorschlag.getUser().getId()).isPresent()) {
+            throw new IllegalArgumentException("Reservierung nicht gefunden oder ID nicht angegeben.");
+        }
+        User user = userService.findById(vorschlag.getUser().getId()).orElse(null);
+        vorschlag.setUser(user);
         return ResponseEntity.ok().body(vorschlagRepository.save(vorschlag));
     }
     public ResponseEntity<List<Vorschlag>> getAllVorschlag() {
